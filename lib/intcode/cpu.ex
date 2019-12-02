999c81ac2a46c04c009c59825ec0352467547c1b
Cameron Price <cameronp@gmail.com>
2019-12-02 14:05:44 -0500

    docs

--- lib/intcode/cpu.ex
+++ lib/intcode/cpu.ex
@@ -2,11 +2,24 @@ defmodule Intcode.Cpu do
   alias Intcode.{Cpu, Memory}
   defstruct memory: nil, ip: 0
 
+  @spec boot(Intcode.Memory.t()) :: Intcode.Cpu.t()
+  @doc """
+  Given a `Memory` with an Intcode program loaded, returns a ready to run `Cpu`
+  """
   def boot(%Memory{} = mem), do: %Cpu{memory: mem, ip: 0}
 
+  @doc """
+  Returns true if the given `Cpu` is in an error state.
+  """
+  @spec error?(Intcode.Cpu.t()) :: boolean
   def error?(%Cpu{memory: %{error: true}}), do: true
   def error?(%Cpu{}), do: false
 
+  @doc """
+  Runs the program in memory, starting from wherever the IP address is located stopping
+  in the event of a halt opcode, or an error.
+  """
+  @spec run(Intcode.Cpu.t()) :: Intcode.Cpu.t()
   def run(%Cpu{ip: :halt} = state), do: %Cpu{state | memory: Memory.succeed(state.memory)}
   def run(%Cpu{ip: :error} = state), do: %Cpu{state | memory: Memory.fail(state.memory)}
 
@@ -16,6 +29,10 @@ defmodule Intcode.Cpu do
     run(new_state)
   end
 
+  @doc """
+  Returns a list containing a memory dump.
+  """
+  @spec dump(Intcode.Cpu.t()) :: [Integer.t()]
   def dump(%Cpu{memory: memory}), do: Memory.to_list(memory)
 
   defp exec(%Cpu{ip: ip, memory: memory} = state, 1) do
