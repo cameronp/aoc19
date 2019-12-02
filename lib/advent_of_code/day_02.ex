213f9bc310f2ebeb0ab1db9bb62864c39ac4ec2f
Cameron Price <cameronp@gmail.com>
2019-12-02 14:40:09 -0500

    extract 'io'

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,37 +1,22 @@
 defmodule AdventOfCode.Day02 do
-  alias Intcode.{Cpu, Memory}
+  alias Intcode.{Cpu, IO, Memory}
 
   def part1(input) do
     input
     |> Memory.load()
-    |> execute_program(12, 2)
+    |> Cpu.boot()
+    |> IO.execute_program(12, 2)
   end
 
   def part2(input) do
     input
     |> Memory.load()
+    |> Cpu.boot()
     |> find_result(19_690_720)
   end
 
-  def find_result(%Memory{} = state, result) do
-    for(n <- 0..255, v <- 0..255, do: {n, v, execute_program(state, n, v)})
+  def find_result(%Cpu{} = state, result) do
+    for(n <- 0..255, v <- 0..255, do: {n, v, IO.execute_program(state, n, v)})
     |> Enum.find(fn {_, _, r} -> r == result end)
-  end
-
-  def execute_program(%Memory{} = state, noun, verb) do
-    result =
-      state
-      |> input_code(noun, verb)
-      |> Cpu.boot()
-      |> Cpu.run()
-
-    case Cpu.error?(result) do
-      true -> :error
-      false -> result.memory[0]
-    end
-  end
-
-  def input_code(%Memory{} = state, a, b) do
-    state |> Memory.poke(1, a) |> Memory.poke(2, b)
   end
 end
--- lib/intcode/io.ex
+++ lib/intcode/io.ex
@@ -1,2 +1,20 @@
 defmodule Intcode.IO do
+  alias Intcode.{Cpu, Memory}
+
+  def execute_program(%Cpu{} = cpu, noun, verb) do
+    cpu_with_input = %{cpu | memory: input_code(cpu.memory, noun, verb)}
+
+    result =
+      cpu_with_input
+      |> Cpu.run()
+
+    case Cpu.error?(result) do
+      true -> :error
+      false -> result.memory[0]
+    end
+  end
+
+  def input_code(%Memory{} = state, a, b) do
+    state |> Memory.poke(1, a) |> Memory.poke(2, b)
+  end
 end
